a2db301215a614be64321154d162d7aa193c6605
crasoke <crasoke@protonmail.com>
2021-11-11 00:29:44 +0100

    updated script

--- eai.sh
+++ eai.sh
@@ -19,11 +19,10 @@ function encryption() {
 }
 
 function base() {
-    #TODO?
-    pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim cryptsetup intel-ucode
-}
-
-function base_config() {
+    pacstrap /mnt base base-devel linux linux-firmware dhcpcd vim cryptsetup 
+    if ! $VM ; then
+        pacstrap /mnt intel-ucode
+    fi
     genfstab -U /mnt > /mnt/etc/fstab
     arch-chroot /mnt echo $HOSTNAME > /etc/hostname
     arch-chroot /mnt echo LANG=en_US.UTF-8 > /etc/locale.conf
@@ -35,10 +34,12 @@ function base_config() {
     done
     locale-gen
     if ! $VM ; then
-        #TODO
+        #TODO complete sed command
         arch-chroot /mnt sed -i "s/TODO/HOOKS=\"base udev autodetect keyboard keymap modconf block encrypt filesystems fsck\"/"
         arch-chroot /mnt mkinitcpio -p linux
     fi
+
+    arch-chroot /mnt systemctl enable --now systemd-timesyncd.service
 }
 
 function create_user() {
@@ -48,37 +49,46 @@ function create_user() {
     gpasswd -a $USER_NAME wheel
 }
 
-function desktop_environment() {
-    pacman_install "xorg-server xorg-xinit i3-wm rofi alacritty"
-    arch-chroot /mnt cp /etc/X11/xinit/xinitrc /home/$USER/.xinitrc
+function tools() {
+    #TODO add more stuff
+    arch-chroot /mnt pacman -Syu --noconfirm git wget
 }
 
 function custom_shell() {
-    pacman_install "zsh"
+    #TODO https://github.com/romkatv/powerlevel10k
+    arch-chroot /mnt pacman -Syu --noconfirm zsh
     arch-chroot /mnt chsh -s /usr/bin/zsh $USER
 }
 
-function graphics() {
-    pacman_install "xf86-video-amdgpu mesa"
+function fonts() {
+    arch-chroot /mnt pacman -Syu --noconfirm noto-fonts
+    #TODO font priority
 }
 
+function sound() {
+    arch-chroot /mnt pacman -Syu --noconfirm pipewire
+    #TODO stuff with DE
+}
+
+function desktop_environment() {
+    #TODO add i3 config and polybar stuff
+    arch-chroot /mnt pacman -Syu --noconfirm xorg-server xorg-xinit i3-wm rofi alacritty
+    arch-chroot /mnt cp /etc/X11/xinit/xinitrc /home/$USER/.xinitrc
+}
+
+function graphics() {
+    arch-chroot /mnt pacman -Syu --noconfirm xf86-video-amdgpu mesa
+}
+
+function applications() {
+    #TODO more applications
+    arch-chroot /mnt pacman -Syu --noconfirm firefox thunderbird gimp
+}
+
+function egpu() {
+    #TODO https://github.com/hertg/egpu-switcher
+}
 
 function main() {
-
+    #TODO everything
 }
-
-
-systemctl enable --now systemd-timesyncd.service
-
-
-
-# tools 
-pacman -S git wget
-# applications
-pacman -S firefox thunderbird gimp 
-
-
-# fonts
-pacman -S noto-fonts (ttf-font-awesome)
-# sound 
-pacman -S pipewire
